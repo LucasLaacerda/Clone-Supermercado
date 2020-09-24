3c0ae9a351c24d47914fb434f461c74513aebf69
Lucas Lacerda <51750552+LucasLaacerda@users.noreply.github.com>
2020-09-24 16:20:37 -0300

    Primeiro commit
    Primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { ShopComponent } from './components/shop/shop.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'cadastro', component: CadastroComponent},
+  { path: 'shop', component: ShopComponent},
+  { path: '**', redirectTo: ''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
